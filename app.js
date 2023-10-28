8c7e95c2a01faac48625d51c4bbf1889fd3fb573
Wei-Hsiang86 <frankflyhuang@gmail.com>
2023-10-28 16:47:48 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const { engine } = require('express-handlebars');
 const app = express()
 const port = 3000
 
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 // 使用 static files
 app.use(express.static('public'));
 
@@ -11,7 +15,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
-  res.send('listing restaurants')
+  res.render('index')
 })
 
 // 設定動態路由
