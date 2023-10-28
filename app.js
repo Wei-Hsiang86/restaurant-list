670f4dd919ed9ba99f359164b0f9860e20d43724
Wei-Hsiang86 <frankflyhuang@gmail.com>
2023-10-28 15:51:41 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// 設定路由
 app.get('/', (req, res) => {
-  res.send('express app for restaurant list')
+  res.redirect('/restaurants')
 })
 
+app.get('/restaurants', (req, res) => {
+  res.send('listing restaurants')
+})
+
+// 設定動態路由
+app.get("/restaurants/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`read restaurant: ${id}`);
+});
+
+// 設定執行伺服器時， CLI 要執行的動作
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 })
